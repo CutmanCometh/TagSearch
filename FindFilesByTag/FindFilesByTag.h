4eb73ccd79867fa8d04872302ec5665db46400f1
Josh Walker <cutmancometh@Reepicheep.local>
2015-04-17 13:41:09 -0600

    Initial Commit

---
+++ FindFilesByTag/FindFilesByTag.h
@@ -0,0 +1,19 @@
+//
+//  FindFilesByTag.h
+//  FindFilesByTag
+//
+//  Created by Josh Walker on 4/17/15.
+//  Copyright (c) 2015 7 September Digital. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FindFilesByTag.
+FOUNDATION_EXPORT double FindFilesByTagVersionNumber;
+
+//! Project version string for FindFilesByTag.
+FOUNDATION_EXPORT const unsigned char FindFilesByTagVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FindFilesByTag/PublicHeader.h>
+
+
